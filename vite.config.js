3b3cb80437df8df01545e759816a6601194005bb
adwickakarsh <adwick0704@gmail.com>
2026-02-18 00:59:30 +0530

    fix

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  base:process.env.VIT_BASE_PATH || '/Portfolio-Web'
 })
