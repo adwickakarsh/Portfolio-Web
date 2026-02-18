e8e2c907cae6290242c8a9a24e2053789aa1b216
adwickakarsh <adwick0704@gmail.com>
2026-02-18 12:15:29 +0530

    preloader

--- src/Components/Preloader.jsx
+++ src/Components/Preloader.jsx
@@ -18,9 +18,9 @@ const Preloader = () => {
   if (step === 5) return null;
 
   return (
-    <div className={`absolute inset-0 bg-blue-200 z-100 transition-all duration-1000 ease-in-out ${step==4 ? 'opacity-0':'opacity-100'}`} >
-      <div className={`absolute inset-0 bg-white z-100 transition-all duration-600 ease-in-out ${step>=2 ? '-translate-y-full':'translate-y-0'}`}></div>
-      <div className={`absolute inset-0 bg-black z-90 transition-all duration-600 ease-in-out ${step>=3 ? '-translate-y-full':'translate-y-0'}`}></div>
+    <div className={`fixed inset-0 bg-blue-200 z-100 transition-all duration-1000 ease-in-out ${step==4 ? 'opacity-0':'opacity-100'}`} >
+      <div className={`fixed inset-0 bg-white z-100 transition-all duration-600 ease-in-out ${step>=2 ? '-translate-y-full':'translate-y-0'}`}></div>
+      <div className={`fixed inset-0 bg-black z-90 transition-all duration-600 ease-in-out ${step>=3 ? '-translate-y-full':'translate-y-0'}`}></div>
     </div>
   )
 }
