942fdb0e8c07954042fee52172c6303058373df6
Adwick <adwick0704@gmail.com>
2026-05-28 22:51:43 +0530

    yes

--- src/Components/Preloader.jsx
+++ src/Components/Preloader.jsx
@@ -18,7 +18,7 @@ const Preloader = () => {
   if (step === 5) return null;
 
   return (
-    <div className={`fixed inset-0 bg-blue-200 z-100 transition-all duration-1000 ease-in-out ${step==4 ? 'opacity-0':'opacity-100'}`} >
+    <div className={`fixed inset-0 bg-purple-200 z-100 transition-all duration-1000 ease-in-out ${step==4 ? 'opacity-0':'opacity-100'}`} >
       <div className={`fixed inset-0 bg-white z-100 transition-all duration-600 ease-in-out ${step>=2 ? '-translate-y-full':'translate-y-0'}`}></div>
       <div className={`fixed inset-0 bg-black z-90 transition-all duration-600 ease-in-out ${step>=3 ? '-translate-y-full':'translate-y-0'}`}></div>
     </div>
