53af09370a941e16c47d4d9378af475430f8b848
adwickakarsh <adwick0704@gmail.com>
2026-06-18 19:47:49 +0530

    project

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -4,27 +4,11 @@ import { useEffect, useRef, useState } from 'react';
 const Project = () => {
   const projects = [
     {
-      title: 'Careers Wesbite',
-      description: 'A full-stack Applicant Tracking System engineered with React and Supabase. It features secure role-based dashboards, dynamic HR hiring pipelines, interview scheduling, and enterprise-grade PostgreSQL database management and document storage.',
-      githubLink: 'https://github.com/adwickakarsh/careers.samify',
-      websiteLink: 'https://careers-samify.vercel.app',
-    },
-    {
-      title: 'Pokémon Data Analytics Pipeline',
-      description: 'Built an end-to-end cloud data pipeline using Airflow, Azure, and Databricks to ingest, transform, and analyze Pokémon data, delivering interactive Power BI dashboards with real-time filtering and performance-optimized analytics.',
-      githubLink: 'https://github.com/adwickakarsh/project-pikachu',
-    },
-    {
       title: 'Weather Dekho',
       description: 'A clean, minimal weather dashboard built with React and Tailwind CSS, featuring real-time data from Open-Meteo, city autocomplete with keyboard navigation, and animated SVG weather icons.',
       githubLink: 'https://github.com/adwickakarsh/Weather-App',
       websiteLink: 'https://weather-dekho-adwick.vercel.app',
-    },
-    {
-      title: 'AdventureWorks Azure Data Pipeline',
-      description: 'Designed a scalable ELT pipeline on Azure using Data Factory, Databricks, and Synapse to process AdventureWorks data, enabling sales and customer insights through optimized transformations and Power BI visualizations.',
-      githubLink: 'https://github.com/adwickakarsh/azureproject',
-    },
+    }
   ];
 
   return (
